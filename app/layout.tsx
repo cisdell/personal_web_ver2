bbc433c3d540b619ea4d72e53f314ae57d8a9e37
Andrew Cho <cisdell@gmail.com>
2024-03-17 16:23:47 -0400

    header in progress

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-
+import Header from "../components/header.tsx";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -19,6 +19,7 @@ export default function RootLayout({
         <div className="bg-[#fbe2e3] relative top-[-6rem] -z-10 right-[11rem] h-[31.23rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dcd9d9] absolute top-[-6rem] -z-10 left-[-35rem] h-[31.23rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         {children}
+        <Header />
       </body>
     </html>
   );
