5254f53d0c524066608f3b49548dddce3113cb89
Andrew Cho <cisdell@gmail.com>
2024-03-16 23:15:24 -0400

    added projects

---
+++ lib/data.ts
@@ -0,0 +1,105 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "First Job",
+    location: "Remote",
+    description:
+      "exp1.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2012",
+  },
+  {
+    title: "Second Job",
+    location: "Loc",
+    description:
+      "exp",
+    icon: React.createElement(CgWorkAlt),
+    date: "2019 - 2021",
+  },
+  {
+    title: "Third Job",
+    location: "Remote",
+    description:
+      "exp",
+    icon: React.createElement(FaReact),
+    date: "2019 - 2021",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "test",
+    description:
+      "exp.",
+    tags: [ "React", "Next.js", "MongoDB", "Tailwind", "Prisma" ],
+    imageUrl: corpcommentImg,
+  },
+  {
+    title: "test",
+    description:
+      "temp.",
+    tags: [ "React", "TypeScript", "Next.js", "Tailwind", "Redux" ],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: [ "React", "Next.js", "SQL", "Tailwind", "Framer" ],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Framer Motion",
+] as const;
