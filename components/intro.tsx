c50e90a27315e00cee7479944a344edabebc2af3
Andrew Cho <cisdell@gmail.com>
2024-03-18 16:33:27 -0400

    button with effects and download works

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,7 +2,11 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
 // import profilepic from "@/public/profilepic.jpg";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -41,12 +45,41 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-      <p>
-        <span className="mb-10 mt-4 px-4 text-2xl font-medium leading-[1.5] sm:text-4xl">
+      <motion.p initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}>
+        <span className="mb-10 pb-10 mt-4 px-4 text-2xl font-medium leading-[1.5] sm:text-4xl">
           Hello! My name is Andrew and I'm a full stack developer! I enjoy
-          building things that are beautiful!
+          building things!
         </span>
-      </p>
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row justify-center items-center gap-2 pt-5 px-4 text-lg"
+        initial={{ opacity: 0, y: 100, scale: 0 }}
+        animate={{ opacity: 1, y: 0, scale: 1 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-2 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10"
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-3 transition" />
+        </Link>
+        <a
+          className="group bg-white px-7 py-2 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 cursor-pointer transition border border-black/10"
+          href="/CV.pdf"
+          download
+        >
+          Download CV
+          <HiDownload className="opacity-70 group-hover:translate-y-2 " />
+        </a>
+        <a className="bg-white px-4 py-3 text-gray-700 flex items-center gap-2 rounded-full">
+          <BsLinkedin />
+        </a>
+        <a className="bg-white px-4 py-3 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem]">
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
