363565f01ffcdaf349b60d6b9e721043c5eaa9f4
Andrew Cho <cisdell@gmail.com>
2024-03-19 15:20:57 -0400

    prop adding

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import SectionHeading from "./section-heading";
-
+import { projectsData } from "../lib/data.ts";
 export default function Projects() {
+  console.log(projectsData);
   return (
     <section>
       <SectionHeading title={"My Projects"} />
-      <div></div>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
     </section>
   );
+}
+
+type ProjectProps = (typeof projectsData)[number];
+
+function Project({ title, description, tags, imageUrl }: ProjectProps) {
+  return <div>{title}</div>;
 }
