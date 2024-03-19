16c8ee11f92c91fffb120f6b1124222677389edb
Andrew Cho <cisdell@gmail.com>
2024-03-19 13:47:18 -0400

    added framer motion for about section

--- components/about.tsx
+++ components/about.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
 
 export default function About() {
   return (
-    <section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
       {/* <SectionHeading>Projects</SectionHeading> */}
       <SectionHeading title={"About"} />
       <p className="mb-3">
@@ -12,6 +20,6 @@ export default function About() {
         software engineer. More intro to come. Please wait for it. I'm a
         software engineer. More intro to come. Please wait for it.
       </p>
-    </section>
+    </motion.section>
   );
 }
