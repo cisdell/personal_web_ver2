ed03bdece100a127fe5a486cb0bc0dd013073b90
Andrew Cho <cisdell@gmail.com>
2024-03-19 14:06:59 -0400

    projects section component start

---
+++ components/projects.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SectionHeading from "./section-heading";
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeading title={"My Projects"} />
+    </section>
+  );
+}
