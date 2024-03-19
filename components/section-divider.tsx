e2c3f44f1c9ea24b1f55135eee65db7f4f44586c
Andrew Cho <cisdell@gmail.com>
2024-03-19 12:49:56 -0400

    changed the horizontal bar to vertical bar

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function SectionDivider() {
   return (
-    <div className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block"></div>
+    <div className="bg-gray-200 my-24 h-1 w-32 rounded-full hidden sm:block"></div>
   );
 }
