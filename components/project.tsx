b5a4ab8f2ff973540e390b3b116f6656496d224e
Andrew Cho <cisdell@gmail.com>
2024-03-19 20:54:50 -0400

    wraped it in a new div

--- components/project.tsx
+++ components/project.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { projectsData } from "@/lib/data";
-import { useScroll } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 import Image from "next/image";
 import { useRef } from "react";
 
@@ -12,31 +12,40 @@ export default function Project({
   tags,
   imageUrl,
 }: ProjectProps) {
-  const ref = useRef(null);
-  useScroll({
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
     target: "",
     offset: ["0 1", "1.33 1"],
   });
+
   return (
-    <section className="group bg-gray-100 max-w-[42rem] border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition">
-      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
-        <h3 className="text-2xl">{title}</h3>
-        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-        <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-          {tags.map((tag, index) => (
-            <li
-              className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-              key={index}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-        <Image
-          src={imageUrl}
-          alt="My Project"
-          quality={95}
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+    <motion.div
+      ref={ref}
+      style={{
+        scale: scrollYProgress,
+        opacity: scrollYProgress,
+      }}
+      className="sm:mb-8 last:mb-0"
+    >
+      <section className="group bg-gray-100 max-w-[42rem] border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3  even:pl-8 hover:bg-gray-200 transition">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+          <h3 className="text-2xl">{title}</h3>
+          <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+            {tags.map((tag, index) => (
+              <li
+                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+                key={index}
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+          <Image
+            src={imageUrl}
+            alt="My Project"
+            quality={95}
+            className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
           transition
           group-hover:scale-[1.04]
           group-hover:-translate-x-3
@@ -47,8 +56,9 @@ export default function Project({
           group-even:group-hover:rotate-2
           group-even:right-[initial]
           group-even:-left-40"
-        />
-      </div>
-    </section>
+          />
+        </div>
+      </section>
+    </motion.div>
   );
 }
