13acf3e33845923e7d092be3d11904cc876be2d9
Andrew Cho <cisdell@gmail.com>
2024-03-21 16:58:45 -0400

    timeline

---
+++ components/experience.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SectionHeading from "./section-heading";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+export default function Experience() {
+  return (
+    <section id="experience">
+      <SectionHeading title={"My experience"} />
+    </section>
+  );
+}
