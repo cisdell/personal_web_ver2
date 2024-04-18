bcbbb7ff6297b2f695ee9bf92d3f34bde2ce6dca
Andrew Cho <cisdell@gmail.com>
2024-04-18 16:43:15 -0400

    resume update

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,7 +93,7 @@ export default function Intro() {
         </Link>
         <a
           className="group bg-white px-7 py-2 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 cursor-pointer transition borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/andrew_cho_resume.pdf"
           download
         >
           Download CV
