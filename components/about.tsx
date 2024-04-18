5cd3274e48ab02715b1f8b157f4536b4f02101d3
Andrew Cho <cisdell@gmail.com>
2024-04-18 16:57:37 -0400

    updated intro language

--- components/about.tsx
+++ components/about.tsx
@@ -30,20 +30,12 @@ export default function About() {
     >
       {/* <SectionHeading>Projects</SectionHeading> */}
       <SectionHeading title={"About Me"} />
-      <p className="mb-3">
-        In the realm of bytes and code, there strides A master of the digital
-        tides, Andrew Cho, with intellect ablaze, In the labyrinth of software,
-        he plays. With keystrokes swift, he weaves his art, Crafting worlds from
-        the bits apart. Lines of logic dance at his command, In the kingdom of
-        tech, he takes his stand. In the silence of the coding night, His screen
-        aglow with pixel light, He delves into the realm unseen, Where dreams of
-        algorithms convene. Through bugs and glitches, he charts his way, A
-        troubleshooter in the break of day. His mind a fortress, his heart a
-        guide, In the endless quest for code refined. With every function, every
-        class, He carves his name in the digital mass. A symphony of ones and
-        zeros, his song, In the grand orchestra of code, he belongs. Andrew Cho,
-        the software sage, Navigating through the digital maze. In ones and
-        zeros, his legacy gleams, A pioneer in the land of digital dreams.
+      <p className="mb-3 text-lg">
+        Hi! My name is Andrew and I am former finance guy turned tech
+        enthusiast. I love solving complex problems and building full stack
+        applications. I live in the metro NYC area and I&apos;m currently
+        looking for opportunities in software engineering roles. <br></br>
+        Let&apos;s have a chat!
       </p>
     </motion.section>
   );
